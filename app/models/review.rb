fabb4c63f01589d93fbdcb9354cc46f29e9f52b5
Nock32 <daisuke.abe.wave@gmail.com>
2021-04-17 23:34:37 +0900

    commit

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :item
   belongs_to :user
+
+  with_options presence: true do
+    validates :title, length: { maximum: 40, too_long: 'タイトルが長すぎます' }
+    validates :content, length: { maximum: 1000, too_long: 'レビュー文が長すぎます' }
+  end
+
 end
