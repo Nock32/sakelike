54e3604dc887d62db8b67fa7f2e5fa90c9aedce6
Nock32 <daisuke.abe.wave@gmail.com>
2021-05-02 12:11:22 +0900

    commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,6 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-# server '35.73.34.160', user: 'ec2-user', roles: %w{app db web}
+server '35.73.34.160', user: 'ec2-user', roles: %w{app db web},ssh_options: {
+  keys: %w(~/.ssh/konkon32.pem)
+}
